63b44507d9753e0844b149e968e5b5377903a20a
Giancarlo Rhodes <giancarlo.rhodes@gmail.com>
2019-01-13 12:05:05 -0600

    first commit

---
+++ CalculatorConsoleApp/CalculatorConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CalculatorConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
