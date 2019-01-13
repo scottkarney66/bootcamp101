849db14b255f12b4c162fb3bd7596a916d674d41
Giancarlo Rhodes <giancarlo.rhodes@gmail.com>
2019-01-13 12:37:05 -0600

    add some classes

---
+++ CalculatorConsoleApp/CalculatorConsoleApp/Calculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CalculatorConsoleApp
+{
+    internal class Calculator
+    {
+        internal static void Add(string firstValue, string secondValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static void Subtract(string firstValue, string secondValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static void Multiply(string firstValue, string secondValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static void Divide(string firstValue, string secondValue)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- CalculatorConsoleApp/CalculatorConsoleApp/Program.cs
+++ CalculatorConsoleApp/CalculatorConsoleApp/Program.cs
@@ -13,7 +13,6 @@ namespace CalculatorConsoleApp
             ////Print out the calculated result.
             ///
 
-
             Console.Write("Please enter a decimal value > ");
             string _firstValue = Console.ReadLine();
             Console.WriteLine();
@@ -25,11 +24,44 @@ namespace CalculatorConsoleApp
             Console.WriteLine("Please enter an operation you want to perform on the two values:");
             Console.Write("ADD - TYPE \"a\" ENTER, SUBTRACT - TYPE \"s\" ENTER, MULTIPLY - TYPE \"m\" ENTER, DIVIDE - TYPE \"d\" > ");
             string _operation = Console.ReadLine();
+            Console.WriteLine();
 
 
+            string _output = "";
 
+            switch (_operation)
+            {
 
+                case "a":
+                    {
+                        Calculator.Add(_firstValue, _secondValue);
+                        break;
+                    }
 
+                case "s":
+                    {
+                        Calculator.Subtract(_firstValue, _secondValue);
+                        break;
+                    }
+
+                case "m":
+                    {
+                        Calculator.Multiply(_firstValue, _secondValue);
+                        break;
+                    }
+
+                case "d":
+                    {
+                        Calculator.Divide(_firstValue, _secondValue);
+                        break;
+                    }
+
+                default:
+                    _output = "did not enter \"a\",\"s\",\"m\",\"d\"";
+                    break;
+            }
+
+            Console.WriteLine(_output);
             Console.ReadKey();
         }
     }
