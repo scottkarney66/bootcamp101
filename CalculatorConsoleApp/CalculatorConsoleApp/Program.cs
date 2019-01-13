5c7a30eb8b36878d61352a1b4fc19cbbe4ee4c39
Giancarlo Rhodes <giancarlo.rhodes@gmail.com>
2019-01-13 12:19:45 -0600

    calc

--- CalculatorConsoleApp/CalculatorConsoleApp/Program.cs
+++ CalculatorConsoleApp/CalculatorConsoleApp/Program.cs
@@ -6,7 +6,31 @@ namespace CalculatorConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //// Write an application that is able to:
+            ////Obtain 2 decimals from the user.
+            ////Obtain one of the following math operations from the user. (Add, Subtract, Multiply or Divide)
+            ////Calculate the result of the chosen math operation using the 2 supplied numbers.
+            ////Print out the calculated result.
+            ///
+
+
+            Console.Write("Please enter a decimal value > ");
+            string _firstValue = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.Write("Please enter another decimal value > ");
+            string _secondValue = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Please enter an operation you want to perform on the two values:");
+            Console.Write("ADD - TYPE \"a\" ENTER, SUBTRACT - TYPE \"s\" ENTER, MULTIPLY - TYPE \"m\" ENTER, DIVIDE - TYPE \"d\" > ");
+            string _operation = Console.ReadLine();
+
+
+
+
+
+            Console.ReadKey();
         }
     }
 }
